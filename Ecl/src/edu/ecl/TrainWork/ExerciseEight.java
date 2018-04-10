4f2219242cecfac113a4ff14bd33bb36b9ce6dfa
Bartosz Kowalski <bartosz.kowalski@eclipsegroup.co.uk>
2018-04-10 13:43:00 +0200

    first commit

---
+++ Ecl/src/edu/ecl/TrainWork/ExerciseEight.java
@@ -0,0 +1,13 @@
+package edu.ecl.TrainWork;
+
+public class ExerciseEight {
+
+    public static void main(String[] args) {
+        int firstFormText = 12;
+        int secondFormText = 13;
+        double firstProductPrice = (double) firstFormText;
+        double secondProductPrice = (double) secondFormText;
+
+        System.out.println("Wartość pierwszej liczby zmiennoprzecinkowej to: " + firstProductPrice);
+    }
+}
