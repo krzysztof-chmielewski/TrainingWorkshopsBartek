5f1ed5bf7ade9bfb67755efe33291662eb1bead7
Bartosz Kowalski <bartosz.kowalski@eclipsegroup.co.uk>
2018-04-10 15:20:59 +0200

    test tracking

--- Ecl/src/edu/ecl/TrainWork/ExerciseEight.java
+++ Ecl/src/edu/ecl/TrainWork/ExerciseEight.java
@@ -3,12 +3,13 @@ package edu.ecl.TrainWork;
 public class ExerciseEight {
 
     public static void main(String[] args) {
-        int firstFormText = 12;
-        int secondFormText = 13;
-        double firstProductPrice = (double) firstFormText;
-        double secondProductPrice = (double) secondFormText;
-        double sadadas = (double) secondFormText;
+        String firstFormText = "12.99";
+        String secondFormText = "13.49";
+
+        double firstProductPrice = Double.parseDouble(firstFormText);
+        double secondProductPrice = Double.valueOf(secondFormText);
 
         System.out.println("Wartość pierwszej liczby zmiennoprzecinkowej to: " + firstProductPrice);
+        System.out.println("Wartość drugiej liczby zmiennoprzecinkowej to: " + secondProductPrice);
     }
 }
