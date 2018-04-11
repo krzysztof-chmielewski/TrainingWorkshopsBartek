3d7ee7e40e3a2551ab16537464043716b5ceb556
Bartosz Kowalski <bartosz.kowalski@eclipsegroup.co.uk>
2018-04-11 08:49:46 +0200

    static problems...

--- Ecl/src/edu/ecl/TrainWork/ExerciseTen.java
+++ Ecl/src/edu/ecl/TrainWork/ExerciseTen.java
@@ -1,27 +1,25 @@
 package edu.ecl.TrainWork;
 
 public class ExerciseTen {
-    public static void main(String[] args) {
-        int[] prices1 = {4, 9};
-        int[] prices2 = new int[2];
+    static int[] prices1 = {4, 9};
+    static int[] prices2 = new int[2];
+
+    static double calc1() {
+        return (double) (prices1[0] + prices1[1]) / prices1.length;
+    }
+
+    static double calc2() {
         prices2[0] = 5;
         prices2[1] = 6;
+        return (double) (prices2[0] + prices2[1]) / prices2.length;
+    }
+
+    public static void main(String[] args) {
 
         System.out.println("Table initialized with values assigned to it, when table is created: " + prices1[0] + ", " + prices1[1]);
         System.out.println("Table initialized with values provided one by one: " + prices2[0] + ", " + prices2[1]);
 
-        /** to działa, ale to nie jest metoda, stąd komentarz :)
-         double calculationAverage1 = (double) (prices1[0] + prices1[1]) / prices1.length;
-         double calculationAverage2 = (double) (prices2[0] + prices2[1]) / prices2.length;
-
-         System.out.println(calculationAverage1);
-         System.out.println(calculationAverage2);
-         */
+        System.out.println("\nAverage calculated from values from first table = " + calc1());
+        System.out.println("Average calculated from values from first table = " + calc2());
     }
-    //TODO nie wiem czemu nie działa mi metoda
-    /**
-    double calc1(int prices1[0], int prices1[1]) {
-        return (prices1[0] + prices1[1]) / prices1.length;
-    }
-    */
 }
